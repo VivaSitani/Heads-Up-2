c042606675be367ce89f5ede140ccfed739a6f98
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2021-08-14 21:54:12 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,114 @@
+#This is starting the game. It sets the score to 0, creates a list of things to act out, and shows the time for each button
+Index = 0
+text_list: List[str] = []
+game.set_score(0)
+text_list = ["Dog",
+    "Cat",
+    "Airplane",
+    "Haircut",
+    "Chew",
+    "Think",
+    "Run",
+    "Golf",
+    "Slap",
+    "Monkey",
+    "Fish",
+    "Snake",
+    "Skate",
+    "Yoga",
+    "Rini",
+    "Stuti",
+    "Navya",
+    "Viva (THE GREAT)",
+    "Harry Potter",
+    "Peeta Mellark",
+    "Ice Cream",
+    "Alex Russo",
+    "Frozen",
+    "Olympics",
+    "DDLJ",
+    "Joey Rooney",
+    "Pant",
+    "Fall",
+    "Dora",
+    "Cake",
+    "Gum",
+    "Car",
+    "Mosquito",
+    "Sloth",
+    "Hopscotch",
+    "TV",
+    "Text",
+    "Sleep",
+    "Brush",
+    "Teacher",
+    "Exercise",
+    "Die",
+    "Slip",
+    "Pizza",
+    "Burger",
+    "Water",
+    "Sky",
+    "Squishy",
+    "Fart",
+    "Bored",
+    "Robot",
+    "Punch",
+    "Ring",
+    "Pig",
+    "Square",
+    "Stare",
+    "River",
+    "Scratch",
+    "Dance",
+    "Fight",
+    "Whine",
+    "Baby",
+    "Shock",
+    "Shop",
+    "Count",
+    "Wait",
+    "Jump",
+    "Billy La Bufanda",
+    "Barbie",
+    "Bitmoji",
+    "GamePigeon",
+    "Potato",
+    "Boogalee!!"]
+basic.show_string("A=60sec B=120sec")
+
+
+#This is button a function. It starts a 60 second countdown.
+def on_button_pressed_a():
+    game.start_countdown(60000)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+
+#This is button b function. It starts a 120 second coundown.
+def on_button_pressed_b():
+    game.start_countdown(120000)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+
+#This is the logo up fuction. It choses a random item from the list, displays its string, and removes it from the list for one round (Same as logo down function). 
+def on_gesture_logo_up():
+    global Index
+    Index = randint(0, len(text_list))
+    basic.show_string("" + (text_list[Index]))
+    text_list.remove_at(Index)
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
+
+#This is the logo down function. It choses a random item from the list, displays its string, and removes it from the list for one round (Same as logo up function). 
+def on_gesture_logo_down():
+    global Index
+    Index = randint(0, len(text_list))
+    basic.show_string("" + (text_list[Index]))
+    text_list.remove_at(Index)
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
+
+
+#This is the screen down fuction. It adds a point to the score indicating the guess was correct. 
+def on_gesture_screen_down():
+    game.add_score(1)
+input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
---
+++ main.ts
@@ -0,0 +1,32 @@
+// This is starting the game. It sets the score to 0, creates a list of things to act out, and shows the time for each button
+let Index = 0
+let text_list : string[] = []
+game.setScore(0)
+text_list = ["Dog", "Cat", "Airplane", "Haircut", "Chew", "Think", "Run", "Golf", "Slap", "Monkey", "Fish", "Snake", "Skate", "Yoga", "Rini", "Stuti", "Navya", "Viva (THE GREAT)", "Harry Potter", "Peeta Mellark", "Ice Cream", "Alex Russo", "Frozen", "Olympics", "DDLJ", "Joey Rooney", "Pant", "Fall", "Dora", "Cake", "Gum", "Car", "Mosquito", "Sloth", "Hopscotch", "TV", "Text", "Sleep", "Brush", "Teacher", "Exercise", "Die", "Slip", "Pizza", "Burger", "Water", "Sky", "Squishy", "Fart", "Bored", "Robot", "Punch", "Ring", "Pig", "Square", "Stare", "River", "Scratch", "Dance", "Fight", "Whine", "Baby", "Shock", "Shop", "Count", "Wait", "Jump", "Billy La Bufanda", "Barbie", "Bitmoji", "GamePigeon", "Potato", "Boogalee!!"]
+basic.showString("A=60sec B=120sec")
+// This is button a function. It starts a 60 second countdown.
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    game.startCountdown(60000)
+})
+// This is button b function. It starts a 120 second coundown.
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    game.startCountdown(120000)
+})
+// This is the logo up fuction. It choses a random item from the list, displays its string, and removes it from the list for one round (Same as logo down function). 
+input.onGesture(Gesture.LogoUp, function on_gesture_logo_up() {
+    
+    Index = randint(0, text_list.length)
+    basic.showString("" + text_list[Index])
+    text_list.removeAt(Index)
+})
+// This is the logo down function. It choses a random item from the list, displays its string, and removes it from the list for one round (Same as logo up function). 
+input.onGesture(Gesture.LogoDown, function on_gesture_logo_down() {
+    
+    Index = randint(0, text_list.length)
+    basic.showString("" + text_list[Index])
+    text_list.removeAt(Index)
+})
+// This is the screen down fuction. It adds a point to the score indicating the guess was correct. 
+input.onGesture(Gesture.ScreenDown, function on_gesture_screen_down() {
+    game.addScore(1)
+})
