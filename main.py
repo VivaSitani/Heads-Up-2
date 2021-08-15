bfc7571ef430bca27725cc286b3ad09840f45098
VivaSitani <66580922+VivaSitani@users.noreply.github.com>
2021-08-15 17:19:41 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
-#This is starting the game. It sets the score to 0, creates a list of things to act out, and shows the time for each button
+# This is button a function. It starts a 60 second countdown.
+def on_button_pressed_a():
+    game.start_countdown(60000)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+# This is the logo up fuction. It choses a random item from the list, displays its string, and removes it from the list for one round.
+def on_gesture_logo_up():
+    global Index
+    Index = randint(0, len(text_list))
+    basic.show_string("" + (text_list[Index]))
+    text_list.remove_at(Index)
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
+# This is the screen down fuction. It adds a point to the score indicating the guess was correct.
+def on_gesture_screen_down():
+    game.add_score(1)
+input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
+
+# This is button b function. It starts a 120 second coundown.
+def on_button_pressed_b():
+    game.start_countdown(120000)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# This is starting the game. It sets the score to 0, creates a list of things to act out, and shows the time for each button.
 Index = 0
 text_list: List[str] = []
 game.set_score(0)
@@ -76,39 +99,3 @@ text_list = ["Dog",
     "Potato",
     "Boogalee!!"]
 basic.show_string("A=60sec B=120sec")
-
-
-#This is button a function. It starts a 60 second countdown.
-def on_button_pressed_a():
-    game.start_countdown(60000)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-
-#This is button b function. It starts a 120 second coundown.
-def on_button_pressed_b():
-    game.start_countdown(120000)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-
-#This is the logo up fuction. It choses a random item from the list, displays its string, and removes it from the list for one round (Same as logo down function). 
-def on_gesture_logo_up():
-    global Index
-    Index = randint(0, len(text_list))
-    basic.show_string("" + (text_list[Index]))
-    text_list.remove_at(Index)
-input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
-
-
-#This is the logo down function. It choses a random item from the list, displays its string, and removes it from the list for one round (Same as logo up function). 
-def on_gesture_logo_down():
-    global Index
-    Index = randint(0, len(text_list))
-    basic.show_string("" + (text_list[Index]))
-    text_list.remove_at(Index)
-input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
-
-
-#This is the screen down fuction. It adds a point to the score indicating the guess was correct. 
-def on_gesture_screen_down():
-    game.add_score(1)
-input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
